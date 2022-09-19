b7fe2ec1376fead842cd148fd80c67640ec7ea1b
Tintgire <boidinhugo14@gmail.com>
2022-09-19 19:44:07 +0200

    commentaire

--- P5-Dev-Web-Kanap-master/front/js/confirmation.js
+++ P5-Dev-Web-Kanap-master/front/js/confirmation.js
@@ -1,4 +1,4 @@
-// Prend l'urlet saute le 1er caractère
+// Prend l'url et saute le 1er caractère
 const orderId = location.search.substring(1);
 document.querySelector("#orderId").textContent = `${orderId}`;
 
